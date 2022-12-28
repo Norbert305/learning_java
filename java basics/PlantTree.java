79b8711e2dd98a2787c7c7eb1980febb62b70c2f
Norbert305 <sealsnorbert@yahoo.com>
2022-12-28 11:47:33 -0500

    my first java program

---
+++ java basics/PlantTree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+public class PlantTree {
+  public static void main(String[] args) {
+      System.out.println("Hi There, I am Norbert and I am learning to code Java!");
+       System.out.println("I am going to plant a tree today!");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  "); 
+    System.out.println("~~~~~~~~~~");
+  }
+}
