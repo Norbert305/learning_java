b3ba36059d656f5bfef8423eac36bbec7d5890da
Norbert305 <sealsnorbert@yahoo.com>
2022-12-28 14:44:36 -0500

    data types java

--- java basics/Variables.java
+++ java basics/Variables.java
@@ -1,8 +1,13 @@
 public class Variables {
 	public static void main(String[] args) {
-    String name = "James Gosling";
-    int yearCreated = 1995;
+    String name = "James Gosling";//strings
+    int yearCreated = 1995; //easy whole numbers
+    double stockShare = 81.7;//decimals, hi or low numbers
+    boolean theSunIsRed = true;//booloeans true or false
+    char myLetterGrade = 'A';//Single characters '!'','A','B','C' must be 'single quotes'
     System.out.println(name);
     System.out.println(yearCreated);
+    System.out.println(stockShare);
+    System.out.println(theSunIsRed);
 	}
 }
