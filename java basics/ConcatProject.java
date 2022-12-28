52ec79893b8e2106edfe585bf3e2fbd9530855c8
Norbert305 <sealsnorbert@yahoo.com>
2022-12-28 17:02:32 -0500

    concat variables project

---
+++ java basics/ConcatProject.java
@@ -0,0 +1,31 @@
+public class ConcatProject {
+    /*
+    This program generates a mad libbed story.
+    Author: Laura
+    Date: 2/19/2049
+    */
+        public static void main(String[] args){
+        
+         String name1 = "Norbert";
+        String adjective1 = "blue";
+        String adjective2 = "hot";
+        String adjective3 = "sick";
+        String verb1 = "fast";
+        String noun1 = "car";
+        String noun2 = "bike";
+        String noun3 = "house";
+        String noun4 = "book";
+        String noun5 = "tree";
+        String noun6 = "plane";
+        String name2 = "Alice";
+        int number = 7;
+        String place1 = "Earth";
+        
+        //The template for the story
+        String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+  
+       
+  
+        System.out.println(story);
+      }       
+  }
