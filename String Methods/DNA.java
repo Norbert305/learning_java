151826c87b27d76739b8d33875edfa1d5b7274fb
Norbert305 <sealsnorbert@yahoo.com>
2023-01-05 15:37:42 -0500

    update

--- String Methods/DNA.java
+++ String Methods/DNA.java
@@ -11,18 +11,18 @@ public class DNA {
     String dna = dna1;
   
    int dnaLength = dna.length();
-    System.out.println(dnaLength);
+    System.out.println(dnaLength);//output: 15
   
     int dnaStart = dna.indexOf("ATG");
     int dnaEnd = dna.indexOf("TGA");
-     System.out.println("Start: "+dnaStart);
-     System.out.println("Stop: "+dnaEnd);
+     System.out.println("Start: "+dnaStart);//output: Start: 0
+     System.out.println("Stop: "+dnaEnd);//output: Stop: 12
   
   if (dnaStart != -1 && dnaEnd != -1) {
-    System.out.println("Condition 1 and 2 are satisfied.");
+    System.out.println("Condition 1 and 2 are satisfied.");//output: Condition 1 and 2 are satisfied.
   }
   if (dnaStart != -1 && dnaEnd != -1 && (dnaEnd - dnaStart) % 3 == 0) {
-    System.out.println("Condition 1 and 2 and 3 are satisfied.");
+    System.out.println("Condition 1 and 2 and 3 are satisfied.");//output: Condition 1 and 2 and 3 are satisfied.
   }
   // String protein = dna.subtracting(3,8);
   // System.out.println("protein avalable: "+protein);
